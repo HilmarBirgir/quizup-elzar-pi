9730353bdf7037377335e043a9074c85bd2b35be
Hilmar Birgir Ólafsson <hilmarbirgir@gmail.com>
2014-08-14 12:24:54 +0000

    derp

--- capture.py
+++ capture.py
@@ -7,8 +7,6 @@ import threading
 camera = picamera.PiCamera()
 picture_path = '/canteen_image/'
 
-run_capture_job()
-
 def capture():
     file_name = os.getcwd() + picture_path + 'image.jpg'
     camera.capture(file_name)
@@ -19,3 +17,4 @@ def run_capture_job():
     capture()
     threading.Timer(5, capture_and_upload).start()
 
+run_capture_job()
