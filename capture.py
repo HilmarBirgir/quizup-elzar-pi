d40c893e6a132674fa58ad5f5783ffe9535fd21d
Hilmar Birgir Ólafsson <hilmarbirgir@gmail.com>
2014-08-14 12:26:51 +0000

    derp

--- capture.py
+++ capture.py
@@ -11,10 +11,9 @@ def capture():
     file_name = os.getcwd() + picture_path + 'image.jpg'
     camera.capture(file_name)
     print('captured image: ' + file_name)
-    upload(file_name)
 
 def run_capture_job():
     capture()
-    threading.Timer(5, capture_and_upload).start()
+    threading.Timer(5, capture).start()
 
 run_capture_job()
