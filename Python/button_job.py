d4ee2302f168b94c3402d186aac5abeda7505e57
Hilmar Birgir Ólafsson <hilmarbirgir@gmail.com>
2014-08-15 11:26:08 +0000

    Added a post function when you press the button.

--- Python/button_job.py
+++ Python/button_job.py
@@ -1,6 +1,10 @@
 import serial
+import requests
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
+
+def post_lunch_is_ready
+    requests.post("http://quizup-elzar.herokuapp.com/lunch")
 
 def run_button_job():
 
@@ -21,5 +25,6 @@ def run_button_job():
         if button_pressed and button_fired == 0:
             button_fired = 1
             print 'Button Pressed!'
+            post_lunch_is_ready()
 
 run_button_job()
